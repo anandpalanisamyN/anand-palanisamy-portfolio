86df80712d7d292d84a95df3ec7a4278862a67e4
anandpalanisamyN <anandpalanisamy.n@gmail.com>
2025-09-10 20:04:07 +0530

    feat(AP-05): Updated vite config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-  },
   base: "/anand-palanisamy-portfolio/",
 });
